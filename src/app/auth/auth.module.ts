0b870c6ffa264463d03ec1834492b7a987adf502
Lewis Matos <lewiismb1@gmail.com>
2021-03-21 17:43:50 -0400

    Rediseño echo!

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
