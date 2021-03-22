ff9f852c00db86f8d1f8bbf0d75e567b0b57a2af
Lewis Matos <lewiismb1@gmail.com>
2021-03-21 20:43:56 -0400

    validaciones 99%

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,28 +8,56 @@ import { FormsModule, FormBuilder, Validators, ReactiveFormsModule, FormGroup } 
 export class RegisterComponent implements OnInit {
 
   formData!: FormGroup
+
   constructor(private fBuilder: FormBuilder) {
     this.createForm()
   }
 
   ngOnInit(): void {
   }
+
+  get invalidName() {
+    return this.formData.get('name')?.invalid && this.formData.get('name')?.touched
+  }
+  get invalidLName() {
+    return this.formData.get('lName')?.invalid && this.formData.get('lName')?.touched
+  }
+  get invalidUser() {
+    return this.formData.get('user')?.invalid && this.formData.get('user')?.touched
+  }
+  get invalidEmail() {
+    return this.formData.get('email')?.invalid && this.formData.get('email')?.touched
+  }
+  get invalidPass1() {
+    return this.formData.get('pass')?.invalid && this.formData.get('pass')?.touched
+  }
+  get invalidPass2() {
+    return this.formData.get('rPass')?.invalid && this.formData.get('rPass')?.touched
+  }
   
   v = Validators
   createForm() {
     this.formData = this.fBuilder.group({
-      name: ['', this.v.required, this.v.minLength(2), this.v.maxLength(30)],
-      lName: ['', this.v.required, this.v.minLength(2), this.v.maxLength(30)],
-      user: ['', this.v.required, this.v.minLength(5), this.v.maxLength(30)],
-      email: ['', this.v.required, this.v.minLength(8), this.v.maxLength(30), this.v.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+/.[a-z]{2,3}$')],
-      pass: ['', this.v.required,this.v.minLength(8), this.v.maxLength(30)],
-      rPass: ['', this.v.required, this.v.minLength(8), this.v.maxLength(30)]
-    }
-    
-  )
-
+      name: ['', [this.v.required, this.v.minLength(2), this.v.maxLength(30)]],
+      lName: ['', [ this.v.required, this.v.minLength(2), this.v.maxLength(30)]],
+      user: ['', [this.v.required, this.v.minLength(5), this.v.maxLength(30)]],
+      email: ['', [this.v.required, this.v.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+/.[a-z]{2,3}$')]],
+      pass: ['', [this.v.required,this.v.minLength(8), this.v.maxLength(30)]],
+      rPass: ['', [this.v.required, this.v.minLength(8), this.v.maxLength(30)]],
+    })
   }
+  
   signUp() {
-    
+    if (this.formData.invalid) {
+      Object.values(this.formData.controls).forEach(control => {
+
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach(control => control.markAllAsTouched())
+        } else {
+
+          control.markAsTouched()
+        }
+      })
+    }
   }
 }
