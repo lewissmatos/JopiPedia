c08d49e22d19dfabb0c6ff3e1055ed5ee552a120
EironDiaz <eirondiaz@gmail.com>
2021-03-23 17:21:17 -0400

    colores card

--- src/app/dashboard/components/card/card.component.ts
+++ src/app/dashboard/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  @Input() bgColor: string = ''
+
+  constructor() { 
+    if (this.bgColor === '') {
+      this.bgColor = '#FFF'
+    }
+  }
 
   ngOnInit(): void {
   }
