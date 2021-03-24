a031d5629a191b4005c8f624940714f3fbe5ddb8
Lewis Matos <lewiismb1@gmail.com>
2021-03-23 21:03:22 -0400

    ...

--- src/app/dashboard/components/card/card.component.ts
+++ src/app/dashboard/components/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() cardData: any = {
+  @Input() public cardData: any = {
     bgColor: '',
     title: '',
     subTitle: '',
--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -12,37 +12,49 @@ export class HomeComponent implements OnInit {
       bgColor: '#F13785',
       title: 'Quimica',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaaa'
     },
     {
       bgColor: '#2980B9',
       title: 'Fisica',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaaa'
     },
     {
       bgColor: '#2ECC71',
       title: 'Biologia',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaa'
     },
     {
       bgColor: '#E74C3C',
       title: 'Matematicas',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaa'
     },
     {
       bgColor: '#F1948A',
       title: 'Mitologia',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaa'
     },
     {
       bgColor: '#5D6D7E',
       title: 'Tecnologia',
       subTitle: 'dsad',
-      desc: ''
+      desc: 'aaaaa'
+    },
+    {
+      bgColor: '#F1C40F',
+      title: 'Astrologia',
+      subTitle: 'dsad',
+      desc: 'aaaaa'
+    },
+    {
+      bgColor: '#A04000',
+      title: 'Historia',
+      subTitle: 'dsad',
+      desc: 'aaaaa'
     },
   ]
 
