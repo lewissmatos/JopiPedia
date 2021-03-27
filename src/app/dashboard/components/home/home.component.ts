a7d48663591e1a5a8dbb833c6e0d2b5b67fb1ed8
Lewis Matos <lewiismb1@gmail.com>
2021-03-26 21:24:27 -0400

    mejroas de la busqeuda

--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -7,7 +7,7 @@ declare const $:any
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
