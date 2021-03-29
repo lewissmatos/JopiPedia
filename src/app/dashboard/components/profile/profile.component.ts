f6f179d663b78769247641f274f53ea9e6c520a7
EironDiaz <eirondiaz@gmail.com>
2021-03-29 10:26:12 -0400

    logOut done

--- src/app/dashboard/components/profile/profile.component.ts
+++ src/app/dashboard/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-profile',
@@ -10,12 +11,26 @@ export class ProfileComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-
-  logOut() {
-    this.router.navigate(['/auth/login'])
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {
+  logOut() {
+    Swal.fire({
+      title: 'Cerrar Sesión',
+      text: "¿Confirma que desea cerrar sesión?",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then(
+      res => {
+      if (res.isConfirmed) {
+        this.router.navigate(['/auth/login'])
+        localStorage.removeItem('token')
+      }
+    })
   }
 
 }
