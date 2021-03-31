7213e2e48b5de1ed44fd0d87ee4be9972c1daa3a
EironDiaz <eirondiaz@gmail.com>
2021-03-31 10:55:50 -0400

    componentes de admin

---
+++ src/app/admin/components/admin-temas/admin-temas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-temas',
+  templateUrl: './admin-temas.component.html',
+  styleUrls: ['./admin-temas.component.css']
+})
+export class AdminTemasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
