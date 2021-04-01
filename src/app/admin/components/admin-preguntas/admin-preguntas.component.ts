d8dd48c866a1d986f2ac73c42cc733497b170514
Lewis Matos <lewiismb1@gmail.com>
2021-03-31 21:37:30 -0400

    diseñando lo de agregar preguntas

--- src/app/admin/components/admin-preguntas/admin-preguntas.component.ts
+++ src/app/admin/components/admin-preguntas/admin-preguntas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { cardData } from 'src/app/dashboard/Models/cardData.model';
+import { ThemesService } from 'src/app/dashboard/services/themes.service';
+import Swal from 'sweetalert2'
+import { AdminPreguntaService } from '../../services/admin-pregunta.service';
 @Component({
   selector: 'app-admin-preguntas',
   templateUrl: './admin-preguntas.component.html',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPreguntasComponent implements OnInit {
 
-  constructor() { }
+  data: cardData = {}
+  
+  allThemes: cardData[] = []
+
+  questionData = {
+    question: '',
+    rc: '',
+    ri1: '',
+    ri2: '',
+    ri3: '',
+  }
+  
+  constructor(private tService: ThemesService, private pService: AdminPreguntaService) {
+    this.getAllThemes()
+    console.log(this.questionData.question)
+  }
 
   ngOnInit(): void {
   }
 
+  getAllThemes() {
+    this.tService.getAllThemes()
+      .subscribe(
+        res => {
+          this.allThemes = res.data
+      }, error =>  console.log(error)
+    )
+  }
+
+  createQuestion(data: any) {
+    this.pService.createPregunta(data)
+      .subscribe(
+        res => {
+          console.log(res.data)
+        }
+      )    
+  }
 }
